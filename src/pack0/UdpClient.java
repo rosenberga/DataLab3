63fe19ce22c3e3c731bc83cdfac0e034431cfffe
geiselc <geiselc@mail.gvsu.edu>
2014-09-24 13:16:16 -0700

    Is it fix?
    The truck have started to move.

--- src/pack0/UdpClient.java
+++ src/pack0/UdpClient.java
@@ -19,7 +19,9 @@ public class UdpClient {
 			DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, Integer.parseInt(portS));
 			clientSocket.send(sendPacket);
 			DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-			System.out.println(new String(receivePacket.getData(), "UTF-8"));
+			clientSocket.receive(receivePacket);
+			String acheivementGet = new String(receivePacket.getData());
+			System.out.println("FROM SERVER:" + acheivementGet);
 			clientSocket.close();
 		} catch (Exception e) {
 			e.printStackTrace();
