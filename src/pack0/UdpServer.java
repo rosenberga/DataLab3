f0743ebfe5de79fd3d450009cddf846b15e1663b
geiselc <geiselc@mail.gvsu.edu>
2014-09-24 12:37:17 -0700

    Server Side Derp Testing
    Our initial shot at getting a functional UDP server, it doesn't do much.

--- src/pack0/UdpServer.java
+++ src/pack0/UdpServer.java
@@ -5,6 +5,28 @@ import java.net.*;
 
 public class UdpServer {
 	public static void main(String[] args){
+		System.out.println("START!");
+		DatagramSocket serverSocket;
+		int port;
 		
+		try {
+			port = Integer.parseInt(args[0]);
+			serverSocket = new DatagramSocket(port);
+			byte[] receiveData = new byte[1024];
+			byte[] sendData = new byte[1024];
+		
+			while(true){
+				DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+				try {
+					serverSocket.receive(receivePacket);
+				} catch (IOException e) {
+					e.printStackTrace();
+					System.out.println("THis is fucking hopeless. Please kill yourself.");
+				}
+			}
+		} catch (SocketException e) {
+			System.out.println("Could not do shit, that makes me angry. DIE!");
+			e.printStackTrace();
+		}
 	}
 }
