6a6db1480ae3167a4a5fcff5c77330ff388062e7
rosenberga <rosenbea@mail.gvsu.edu>
2014-09-24 12:46:25 -0400

    client

--- src/pack0/UdpClient.java
+++ src/pack0/UdpClient.java
@@ -5,6 +5,24 @@ import java.net.*;
 
 public class UdpClient {
 	public static void main(String[] args) {
-		
+		BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+		String ip;
+		try {
+			ip = inFromUser.readLine();
+			String portS = inFromUser.readLine();
+			DatagramSocket clientSocket = new DatagramSocket();
+			InetAddress IPAddress = InetAddress.getByName(ip);
+			byte[] sendData = new byte[1024];
+			byte[] receiveData = new byte[1024];
+			String data = inFromUser.readLine();
+			sendData = data.getBytes();
+			DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, Integer.parseInt(portS));
+			clientSocket.send(sendPacket);
+			DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+			System.out.println(receivePacket.getData());
+			clientSocket.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
