9f512ffc858a43967c4843703026e6260d61efa7
rosenberga <rosenbea@mail.gvsu.edu>
2014-09-28 20:28:51 -0400

    put dig in a thread

--- src/pack0/UdpClient.java
+++ src/pack0/UdpClient.java
@@ -5,28 +5,37 @@ import java.net.*;
 
 public class UdpClient {
 	public static void main(String[] args) {
-		BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+		BufferedReader inFromUser = new BufferedReader(new InputStreamReader(
+				System.in));
 		String ip;
 		try {
 			System.out.print("Enter ip address: ");
 			ip = inFromUser.readLine();
 			System.out.print("Enter port ");
 			String portS = inFromUser.readLine();
-			DatagramSocket clientSocket = new DatagramSocket();
-			clientSocket.setSoTimeout(9000);
-			InetAddress IPAddress = InetAddress.getByName(ip);
-			byte[] sendData = new byte[1024];
-			byte[] receiveData = new byte[1024];
-			System.out.print("Enter a message ");
-			String data = inFromUser.readLine();
-			sendData = data.getBytes();
-			DatagramPacket sendPacket = new DatagramPacket(sendData, data.length(), IPAddress, Integer.parseInt(portS));
-			clientSocket.send(sendPacket);
-			DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-			clientSocket.receive(receivePacket);
-			String message = new String(receivePacket.getData());
-			System.out.println("FROM SERVER:" + message);
-			clientSocket.close();
+			while (true) {
+				DatagramSocket clientSocket = new DatagramSocket();
+				clientSocket.setSoTimeout(9000);
+				InetAddress IPAddress = InetAddress.getByName(ip);
+				byte[] sendData = new byte[1024];
+				byte[] receiveData = new byte[1024];
+				System.out.print("Enter a message ");
+				String data = inFromUser.readLine();
+				if(data.equalsIgnoreCase("quit")) {
+					clientSocket.close();
+					break;
+				}
+				sendData = data.getBytes();
+				DatagramPacket sendPacket = new DatagramPacket(sendData,
+						sendData.length, IPAddress, Integer.parseInt(portS));
+				clientSocket.send(sendPacket);
+				DatagramPacket receivePacket = new DatagramPacket(receiveData,
+						receiveData.length);
+				clientSocket.receive(receivePacket);
+				String message = new String(receivePacket.getData());
+				System.out.println("FROM SERVER:" + message);
+				clientSocket.close();
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
--- src/pack0/UdpServer.java
+++ src/pack0/UdpServer.java
@@ -4,47 +4,98 @@ import java.io.*;
 import java.net.*;
 
 public class UdpServer {
+	private int port;
+	private DatagramSocket serverSocket;
 	public static void main(String[] args){
-		DatagramSocket serverSocket = null;
-		int port;
-		
+		new UdpServer();
+	}	
+	public UdpServer() {
 		InetAddress hostAddress;
 		try {
 			hostAddress = InetAddress.getLocalHost();
 			System.out.println("The host address is: " + hostAddress);
 		} catch (UnknownHostException e1) {
 			e1.printStackTrace();
-			System.out.println("GET THE FUCK OUTTA HERE!");
+			System.out.println("Failed to get host address");
 		}
 		
 		try {
-			port = Integer.parseInt(args[0]);
-			serverSocket = new DatagramSocket(9876);
+			port = 9876;
+			serverSocket = new DatagramSocket(port);
 		
 			while(true){
 				byte[] receiveData = new byte[1024];
-				byte[] sendData = new byte[1024];
 				DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
 				try {
 					serverSocket.receive(receivePacket);
 					String sentence = new String(receivePacket.getData());
+					if(sentence.equalsIgnoreCase("quit")) {
+						if(serverSocket != null) {
+							serverSocket.close();
+						}
+						break;
+					}
 					System.out.println("RECEIVED: " + sentence);
-					InetAddress IPAddress = receivePacket.getAddress();
-					String newMessage = sentence +"\n";
-					sendData = newMessage.getBytes();
-					DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
-					serverSocket.send(sendPacket);
+					Dig d = new Dig(sentence, receivePacket.getAddress());
+					d.run();
 				} catch (IOException e) {
 					e.printStackTrace();
-					System.out.println("This is fucking hopeless. Please kill yourself.");
+					System.out.println("IOException");
 				}
 			}
 		} catch (SocketException e) {
-			System.out.println("Could not do shit, that makes me angry. DIE!");
+			System.out.println("Socket Exception");
 			e.printStackTrace();
-		} finally{
-			if(serverSocket != null) {
-				serverSocket.close();
+		}
+	}
+	private class Dig extends Thread {
+		private String request;
+		private String command;
+		private BufferedReader reader;
+		private StringBuilder sb;
+		private String result;
+		private InetAddress ip;
+		private byte[] sendData;
+		
+		public Dig(String r, InetAddress ipA) {
+			request = r;
+			char[] chars = request.toCharArray();
+			command = "dig ";
+			for(int i = 0; i < chars.length; i++){
+				char c = chars[i];
+				if(c != '\0') {
+					command = command + c;
+				}
+			}
+			
+			sendData = new byte[1024];
+			ip = ipA;
+		}
+		
+		public void run() {
+			try{
+				Process p = Runtime.getRuntime().exec(command);
+				p.waitFor();
+				reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
+				String line = "";
+				sb = new StringBuilder();
+				while((line = reader.readLine()) != null) {
+					sb.append(line +"\n");
+				}
+				result = sb.toString();
+				sendData = result.getBytes();
+				DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, ip, port);
+				serverSocket.send(sendPacket);
+			} catch(Exception e) {
+				e.printStackTrace();
+			} finally {
+				if (reader != null) {
+					try {
+						reader.close();
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
 			}
 		}
 	}
