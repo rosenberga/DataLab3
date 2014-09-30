4cc1ca4946ae8cd18e7e5992b3b06e1a34ffb597
geiselc <geiselc@mail.gvsu.edu>
2014-09-30 10:22:28 -0400

    Default UDPServer

---
+++ udpserver.java
@@ -0,0 +1,24 @@
+package pack0;
+
+import java.io.*;
+import java.net.*;
+
+class udpserver{
+    public static void main(String args[]) throws Exception{
+	DatagramSocket serverSocket = new DatagramSocket(9876);
+	while(true){
+	    byte[] recvData = new byte[1024];
+	    byte[] sendData = new byte[1024];
+	    DatagramPacket recvPacket = 
+		new DatagramPacket(recvData,recvData.length);
+	    serverSocket.receive(recvPacket);
+	    String message = new String(recvPacket.getData());
+	    InetAddress IPAddress = recvPacket.getAddress();
+	    int port = recvPacket.getPort();
+	    String newMessage  = "From Server" + message+"\n";
+	    sendData = newMessage.getBytes();
+	    DatagramPacket sendPacket = new DatagramPacket(sendData,sendData.length,IPAddress,port);
+	    serverSocket.send(sendPacket);
+	}
+    }
+}
