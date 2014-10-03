48025c87b896fe8b2903f85ba24330aba1a57b83
rosenberga <rosenbea@mail.gvsu.edu>
2014-10-03 13:15:04 -0400

    Create Cache.java

---
+++ src/pack0/Cache.java
@@ -0,0 +1,31 @@
+package pack0;
+//test
+public class Cache {
+	private int timeToLive;
+	private String ipAddress;
+	private String name;
+	public Cache(int timeToLive, String ipAddress, String name) {
+		super();
+		this.timeToLive = timeToLive;
+		this.ipAddress = ipAddress;
+		this.name = name;
+	}
+	public int getTimeToLive() {
+		return timeToLive;
+	}
+	public void setTimeToLive(int timeToLive) {
+		this.timeToLive = timeToLive;
+	}
+	public String getIpAddress() {
+		return ipAddress;
+	}
+	public void setIpAddress(String ipAddress) {
+		this.ipAddress = ipAddress;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
